88990d0dfd361a6c34d69e659199ec2df79ba3c0
Pozhan-js <854059946@qq.com>
2023-04-04 08:33:05 +0800

    feat: 开始制作首页

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -8,36 +8,45 @@
 
     <!-- menu部分 -->
     <div class="menu">
-      <el-menu default-active="2">
+      <el-menu default-active="2" background-color="#001529" text-color="#ccc">
+        <!-- 系统总览 -->
         <el-sub-menu index="1">
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
           </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+        <!-- 系统总览 -->
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+
+        <!-- 系统总览 -->
+        <el-sub-menu index="3">
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+
+        <!-- 系统总览 -->
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
       </el-menu>
     </div>
   </div>
